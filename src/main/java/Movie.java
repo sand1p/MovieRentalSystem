d795572b723fd8cc4085880bca077a0511b04a11
Sandip gatkal <GS-1275@GSLAB.COM>
2019-08-29 03:11:12 +0530

    Moved getCharge method from Movie class to Price classes

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -36,37 +36,12 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        //determine amounts for each line
-        double result = 0;
-        switch (getPriceCode()) {
-            case REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-
-            case CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-
-            case NEW_RELEASE:
-                result += 1.5;
-                System.out.println("This Ammount add 1.5: " + result);
-                result += daysRented * 3;
-                System.out.println("This Ammount 3*3 = 9 + 1.5: " + result);
-                break;
-        }
-        System.out.println("This Ammount: " + result);
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
         //add Bonus for a two day new release rental
-        if ((_price.getPriceCode() == Movie.NEW_RELEASE)
-                && daysRented > 2) return 2;
-        else return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 
 
--- src/main/java/Price.java
+++ src/main/java/Price.java
@@ -1,21 +1,54 @@
 abstract class Price {
     abstract int getPriceCode();
+
+    abstract public double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        //add Bonus for a two day new release rental
+        if ((getPriceCode() == Movie.NEW_RELEASE)
+                && daysRented > 2) return 2;
+        else return 1;
+    }
 }
 
 class ChildrensPrice extends Price {
-   int getPriceCode(){
-       return Movie.CHILDRENS;
-   }
+    int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+        return result;
+    }
 }
 
 class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double result = 1.5;
+        result += daysRented * 3;
+        return result;
+    }
 }
 
-class RegularPrice extends  Price {
+class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
+    }
 }
+
